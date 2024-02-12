47ef963094c798bc520567c3a24b53cf119261f4
oguzhancbc <cbcoguzhan@hotmail.com>
2024-02-12 06:22:28 +0300

    Add project files.

---
+++ Business/Concrete/PersonManager.cs
@@ -0,0 +1,48 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Business.Abstract;
+using MernisServiceReferance;
+using MernisServiceReferance;
+
+namespace Business.Concrete
+{
+    //Çıplak class kalmasın
+    public class PersonManager : IApplicantService
+    {
+        //encapsulation
+        public void ApplyForMask(Person person)
+        {
+
+        }
+
+        public List<Person> GetList()
+        {
+            return null;
+        }
+
+        public bool CheckPerson(Person person)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+
+            return client.TCKimlikNoDogrulaAsync(
+                new TCKimlikNoDogrulaRequest
+                (new TCKimlikNoDogrulaRequestBody(person.NationalIdentity, person.FirstName, person.LastName, person.DateOfBirthday)))
+                .Result.Body.TCKimlikNoDogrulaResult;
+
+        }
+
+        public void ApplyFormask(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Person> Getlist()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+}
---
+++ Workaround/Program.cs
@@ -0,0 +1,42 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+namespace Workaround
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Selamver(isim:"Oğuzhan");
+            Selamver();
+            //diziler / arrays
+            string[] ogrenciler = new string[3];
+                ogrenciler[0] = "engin";
+                ogrenciler[0] = "kerem";
+                ogrenciler[0] = "Oğuzhan";
+                ogrenciler[0] = "Ahmet";
+            for (int i = 0; i < ogrenciler.Length; i++)
+            {
+                Console.WriteLine(ogrenciler[i]);
+            }
+            Person person1 = new Person();
+            person1.FirstName = "Oğuzhan";
+            person1.LastName = "Cebeci";
+            person1.DateOfBirthday = 1998;
+            person1.NationalIdentity = 123;
+
+            Person person2 = new Person();
+            person2.FirstName = "Engin";
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+
+        }
+        static void Selamver(string isim="Kullanıcı") 
+        {
+            Console.WriteLine("merhaba"+" " + isim); 
+        }
+        
+    }
+}
